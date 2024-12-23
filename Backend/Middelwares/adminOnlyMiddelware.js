0f643ff84394107cc7ab8ba8b6ad54a02368b178
Umesh Singh <umeshsinghmehta4@gmail.com>
2024-12-23 14:21:07 +0530

    feat: Add documentation for adminOnly middleware to clarify its purpose and parameters

--- Backend/Middelwares/adminOnlyMiddelware.js
+++ Backend/Middelwares/adminOnlyMiddelware.js
@@ -1,3 +1,12 @@
+/**
+ * @function adminOnly
+ * @description Middleware to check if user is an admin
+ * @param {object} req - request object
+ * @param {object} res - response object
+ * @param {function} next - next function to be called
+ * @returns {object} - returns response object
+ */
+
 const adminOnly = (req,res,next)=>{
     if(req.user.role!='admin'){
         return res.status(403).json({message:"Access Denied: Admin privileges required."})
