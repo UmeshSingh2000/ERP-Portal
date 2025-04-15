65d4b0dde2fd54b0df22bda57e0370d0c37031ca
Umesh Singh <umeshsinghmehta4@gmail.com>
2025-04-15 23:54:39 +0530

    feat: enhance StudentAttendance and StudentHome components; improve structure and styling for better user experience

--- Front/src/Pages/Student/StudentAttendance.jsx
+++ Front/src/Pages/Student/StudentAttendance.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const StudentAttendance = () => {
     return (
-        <div>
-            See your attendance
-        </div>
+        <main>
+            <header className="flex flex-col gap-4 p-4 md:p-6 lg:p-10">
+                <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">Attendance</h1>
+                <p className="text-sm md:text-base text-gray-600 font-medium">
+                    See Your Attendance
+                </p>
+            </header>
+        </main>
     )
 }
 
--- Front/src/Pages/Student/StudentHome.jsx
+++ Front/src/Pages/Student/StudentHome.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const StudentHome = () => {
   return (
-    <div>
-      student home
-    </div>
+    <main>
+      <header className="flex flex-col gap-4 p-4 md:p-6 lg:p-10">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">Dashboard</h1>
+        <p className="text-sm md:text-base text-gray-600 font-medium">View Your Stats!!</p>
+      </header>
+    </main>
   )
 }
 
